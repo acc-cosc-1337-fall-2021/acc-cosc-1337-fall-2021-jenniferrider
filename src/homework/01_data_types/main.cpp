7c0421c0b2cb0b13bce8ceb070268156701050f7
jenniferrider <jennifer.rider@g.austincc.edu>
2021-09-08 21:28:34 -0500

    Homework 1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,7 +1,9 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
+using std::cout; using std::cin;
 
 
 /*
@@ -10,6 +12,21 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 int main()
 {
 	
+	// variables
+	int num;
+	int num1;
+	int result;
 
+	cout << "Enter a number: ";
+	cin >> num;
+
+	// multiply mark one
+	result = multiply_numbers(num);
+	cout << "First Result: " << result << "\n";
+
+	// multiply mark two
+	num1 = 4;
+	result = multiply_numbers(num1);
+	cout << "Second Result: " << result << "\n";
 	return 0;
 }
