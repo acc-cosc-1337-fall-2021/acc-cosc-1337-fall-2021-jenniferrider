a6c092af15ae4031e81e4a36f7148c9baa441aac
jenniferrider <jennifer.rider@g.austincc.edu>
2021-09-12 02:20:51 -0500

    Program completed with math.h addition to correct mistake in total

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,12 +1,22 @@
-#include "expressions.h"
+// includes
+#include "hwexpressions.h"
 
-//example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}
+// constants
+const double TAX_RATE = 6.75;
 
 //write function code here
+//functions
+double get_sales_tax_amount(double meal_amount)
+{
+    //get product
+    return meal_amount * (TAX_RATE/100);
+}
+
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+    //get product
+    return meal_amount * (tip_rate/100);
+}
 
 
 
