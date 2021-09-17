5738113222831a202c7f24a91ffdd8efb2ae1b87
jenniferrider <jennifer.rider@g.austincc.edu>
2021-09-17 18:21:03 -0500

    Commiting homework 3 changes

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,60 @@
 //write include statement for decisions header
-
+#include <decisions.h>
+#include <stdio.h>
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    // if function decision tree to get grade
+    if (grade >= 90)
+    {
+        return "A";
+    }
+    else
+    {
+        if (grade <= 89 && grade >= 80)
+        {
+            return "B";
+        }
+        else
+        {
+            if (grade <= 79 && grade >= 70)
+            {
+                return "C";
+            }
+            else
+            {
+                if(grade <=69 && grade >= 60)
+                {
+                    return "D";
+                }
+                else
+                {   
+                    return "F";
+                }
+            }
+        }
+    }
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    // switch function used to get grade
+    switch(grade)
+    {
+        case 90 ... 100:
+            return "A";
+            break;
+        case 80 ... 89:
+            return "B";
+            break;
+        case 70 ... 79:
+            return "C";
+            break;
+        case 60 ... 69:
+            return "D";
+            break;
+        default:
+            return "F";
+    }
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,63 @@
 //write include statements
+#include <iostream>
+#include <decisions.h>
+#include <stdio.h>
+
+// namespaces
+using std::cout; using std::cin;
 
 int main() 
 {
+	// variables
+	int num;
+	int grade;
+	string letter_grade;
+
+	// menu to select your poision
+	cout << "\n\nMAIN MENU\n\n";
+	cout << "1-Letter Grade Using If\n";
+	cout << "2-Letter Grade Using Switch\n";
+	cout << "3-Exit\n\n";
+	cout << "Enter your selection from the menu: ";
+	cin >> num;
+
+	switch(num)
+	{
+		case 1 ... 2:
+			// get user input for numerical grade
+			cout << "Enter a numerical grade between 0 and 100: ";
+			cin >> grade;
+
+			if (grade < 0 || grade > 100)
+			{
+				cout << "\nThe number you entered is out of range. This program will now exit.\n";
+			}
+			else
+			{
+				switch (num)
+				{
+					case 1:
+						cout << "\nYou chose the 'if' method of letter grade calculation.\n";
+						letter_grade = get_letter_grade_using_if(grade);
+						break;
+					default:
+						cout << "\nYou chose the 'switch' method of letter grade calculation.\n";
+						letter_grade = get_letter_grade_using_switch(grade);
+						break;
+				}
+
+				cout << "\nThe numerical grade " << grade << " is the letter grade " << letter_grade << ".\n";
+			}
+			break;
+		case 3:
+			cout << "\nYou chose to exit the program\n.";
+			break;
+		default: 
+			cout << "\nYou made an invalid selection so this program is ending.\n";
+			break;
+	}
+
+	cout << "Goodbye.\n";
+	
 	return 0;
 }
